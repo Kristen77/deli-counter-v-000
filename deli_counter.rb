3e301f885549128c81861e6bcd34d42f218be4db
Kristen Robertson <kristen77@gmail.com>
2018-10-19 16:28:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli, name)
+  if katz_deli = []
+  puts "The line is currently empty."
+    else puts "#{name}. #{katz_deli.length}"
+  end
+end
+
+def take_a_number(katz_deli, customer)
+
+end
