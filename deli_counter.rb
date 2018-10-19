edfdd535636d2a710386530c689d4070840aa566
Kristen Robertson <kristen77@gmail.com>
2018-10-19 16:34:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli, name)
+def line(katz_deli)
   line_position = []
     if katz_deli.empty?
   puts "The line is currently empty."
     else 
       counter = 1 
+        katz_deli.each do |customer|
       puts "#{name}. #{katz_deli.length}"
   end
 end
